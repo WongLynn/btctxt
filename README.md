35fab372e18ef3785d1ef9685566562012db39bb
dmercer <mercer321@gmail.com>
2014-02-14 23:51:30 -0500

    Updated to expand launch options and available arguments

--- README.md
+++ README.md
@@ -1,23 +1,113 @@
-This python script may be used to monitor the price of bitcoin and send email alerts when the price has changed significantly.
+------------------------------------------------------
+------------------------BTCtxt------------------------
+------------------------------------------------------
+
+BTCtxt monitors the price of BTC by grabbing the 
+current price from bitcoinity and sending an email
+alert when the price has changed significantly. Since
+cellphones have a corresponding email address, BTCtxt
+can submit an smtp message as an sms message to your
+phone.
+
+------------------------------------------------------
+---------------------Dependencies---------------------
+------------------------------------------------------
+
+The only nonstandard library used in this application
+is the BeautifulSoup module. In the next release, I'm
+going to try to remove this dependency so that the
+entirity of this code is either a standard library or
+easily readable code. 
+
+------------------------------------------------------
+--------------------Calling BTCtxt--------------------
+------------------------------------------------------
+
+(Option 1) Fully from the commandline
+Example: 
+$ btctxt.py ~/path/to/price username@gmail.com user pw smtp.server.com port 5551234567@cell.com .1 180
+
+Arg 1: Provide a path to a valid directory in which 
+you'd like to store a blank file. If a file does not 
+exist, BTCtxt will create a fill with value 1. This 
+guarantees you'll receive a txt the first time BTCtxt 
+successfully runs.
+
+Arg 2: The email address from which you'd like to send
+       the sms message.
+
+Arg 3: Your email username.
+
+Arg 4: Your email password.
+
+Arg 5: Your SMTP server. For gmail, it's smtp.gmail.com
+
+Arg 6: Your SMTP server's port. For gmail, it's port 
+       587.
+
+Arg 7: The email address you wish to send your alert 
+       to. For verizon it is [9digitnumber]@vtext.com
+
+Arg 8: The proportional change in price you'd wish 
+       to be notified at. 0.1 implies BTCtxt will 
+       view a 10% change in price as an alert worthy 
+       event.
+
+Arg 9: The amount of time (in seconds) BTCtxt should 
+       wait between checking the price. 180 implies 
+       180 seconds between checks.
+
+Args 10 and 11: Which exchange you want to monitor 
+     (10) and using which currency (11). These values
+     will be substituted into the string concatanation:
+"http://bitcoinity.org/markets/" + exchange + "/" + currency.
+     So, be sure to verify you are producing a valid
+     URL using your provided arguments by visiting 
+     bitcoinity beforehand.
+
+Arg 12: Choose whether you wanted to monitor the last 
+        bid price (by providing "buy" as your argument) 
+        or the last ask price (by providing "sell" as 
+        your argument).
+
+(Option 2) Partly from a config file/ partly from commandline
+Example:
+$ btctxt.py ./btctxt.conf
+Username: myUsername
+Password: 
+
+This option is intended to be a compromise of convenience
+and security (i.e., not leaving a .conf file or bash script
+lying around with your email credentials in it).
+
+Arg 1: Path to your .config file. See example.conf for details
+Python input 1: Enter your email username
+Python input 2: Enter your email password (hides as you type)
+
+The rest of the information will either be drawn from your 
+.conf file or be set to default values if no other value
+is provided.
+
+(Option 3) Fully from a config file
+Example:
+$ btctxt.py ./btctxt.conf c
+
+Arg 1: Path to your .config file. See example.conf for details
+Arg 2: The letter c, for credentials I suppose.
+
+This option assumes you've added something like the following 
+section to your .conf file.
+
+[Credentials]
+user = myUsername
+pw = myPassword
+
+Just to be clear here: if you decide to go with option 3, 
+you're deciding to keep your credentials in plain text
+on your pc at your own risk.
 
 
-btctxt.py ~/var/btctxt/price username@gmail.com username password smtp.gmail.com 587 5551234567@vtext.com .1 180
 
-Step 1: Set the first argument, "path", to the path to you'd like to keep your price log. The price log is a file that simply contains the last price that was notified to the user. In the example, this file is stored at ~/var/price.
 
-Step 2: Set the second argument, "from_address". This is simply your email address.
 
-Step 3: Set the third argument, "user". This is the username portion of your email server's login credentials. In the example script, the username is creatively named "username".
-
-Step 4: Set the fourth argument, "pw". Similar to (Step 3), this is the password portion of your email server's login credentials.
-
-Step 5: Set the fifth argument, "SMTP". This is the SMTP address of your email server. For gmail, it is smtp.gmail.com
-
-Step 6: Set the sixth argument, "port". This is the port of your email server. For gmail, it is port 587.
-
-Step 7: Set the seventh argument, "to_address". This is your the email address that corresponds to your cell providers sms server. For verizon it [9digitnumber]@vtext.com
-
-Step 8: Set the eighth argument, "ratio". This is the proportional change in price at which btctxt should send you an email. In the example, we would want to be notified at a 10% change in price.
-
-Step 9: Set the nineth argument, "sleepTime". This is the amount of time (in seconds) btctxt should wait between checking the price of btc. In the example, btctxt waits 180 seconds between checking the price.
 
--- btctxt.py
+++ btctxt.py
@@ -3,48 +3,39 @@
 #import libraries
 import sys
 import os
+import getpass
+import ConfigParser
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import time
 from email.mime.text import MIMEText
 import smtplib
 
-#read stdin
-path = sys.argv[1]
-from_address = sys.argv[2]
-user = sys.argv[3]
-pw = sys.argv[4]
-smtp = sys.argv[5]
-port = sys.argv[6]
-to_address = sys.argv[7]
-ratio = float(sys.argv[8])
-sleepTime = float(sys.argv[9])
 
-
-def main(path, from_address, user, pw, smtp, port, to_address, ratio):
+def main(path, from_address, user, pw, smtp, port, to_address, ratio, exchange, currency, buy_sell):
         #get current price
-        web = urllib2.urlopen("http://bitcoinity.org/markets/bitstamp/USD").read()
+        web = urllib2.urlopen("http://bitcoinity.org/markets/" + exchange + "/" + currency).read()
         soup = BeautifulSoup(''.join(web))
-        last = float(soup.find('span', id="last_sell").string)
+        last = float(soup.find('span', id="last_" + buy_sell).string)
         #check last price
         checkLogExistence(path)
         logP = getLog(path)
         change = abs((logP-last)/last)
         if change > ratio:
-                notify(last, path, from_address, user, pw, smtp, port, to_address)
+                notify(last, currency, exchange, path, from_address, user, pw, smtp, port, to_address)
         return None
 
 
-def notify(last, path, from_address, user, pw, smtp, port, to_address):
-        sendEmail(last, from_address, user, pw, smtp, port, to_address)
+def notify(last, currency, exchange, path, from_address, user, pw, smtp, port, to_address):
+        sendEmail(last, currency, exchange, from_address, user, pw, smtp, port, to_address)
         writeLog(last, path)
         return None
 
 
-def sendEmail(last, from_address, user, pw, smtp, port, to_address):
+def sendEmail(last, currency, exchange, from_address, user, pw, smtp, port, to_address):
         #Format email_content
         timestr = time.strftime("%H:%M", time.localtime())
-        email_content = "The price of BTC is currently " + str(last) + " as of " + timestr + " on Bitstamp."
+        email_content = "The price of BTC is currently " + str(last) + currency + " as of " + timestr + " on " + exchange + "."
         #Create msg
         msg = MIMEText(email_content)
         msg['Subject'] = "BTCtxt"
@@ -84,6 +75,81 @@ def checkLogExistence(path):
         return None
 
 
+def getConf(path):
+        parser = ConfigParser.RawConfigParser()
+        parser.read(path)
+        necessary = ["path", "smtp", "port", "to", "from"]
+        optional = ["exchange", "currency", "buy_sell", "sleeptime", "sleeptime"]
+        #check necessary
+        if len(set(parser.options("Necessary"))) == 5:
+                for item in necessary:
+                        if item not in parser.options("Necessary"):
+                                return False
+        else:
+                print "You've provided too many parameters in the [Necessary] section. Please include an entry for all of the following: path, smtp, port, to, from."
+        #check optional
+        if len(set(parser.options("Optional"))) <= 3:
+                for item in optional:
+                        if item not in parser.options("Optional"):
+                                if item is "exchange":
+                                        parser.set("Optional", item, "bitstamp")
+                                elif item is "currency":
+                                        parser.set("Optional", item, "USD")
+                                elif item is "buy_sell":
+                                        parser.set("Optional", item, "sell")
+                                elif item is "sleeptime":
+                                        parser.set("Optional", item, "180")
+                                else:
+                                        parser.set("Optional", item, "0.075")
+        else:
+                print "You've provided too many parameters in the [Optional] section of your .conf . Please include any subset of the following: exchange (bitstamp), sleeptime (180), ratio (0.075). If any argument is not included, the paranthetical default value will be used."
+        return parser
+
+if len(sys.argv) == 2:
+        #read .conf
+        parser = getConf(sys.argv[1])
+        path = os.path.expanduser(parser.get("Necessary", "path"))
+        from_address = parser.get("Necessary", "from")
+        smtp = parser.get("Necessary", "smtp")
+        port = parser.get("Necessary", "port")
+        to_address = parser.get("Necessary", "to")
+        ratio = float(parser.get("Optional", "ratio"))
+        sleepTime = float(parser.get("Optional", "sleeptime"))
+        exchange = parser.get("Optional", "exchange")
+        currency = parser.get("Optional", "currency")
+        buy_sell = parser.get("Optional", "buy_sell")
+        #get user and pw from stdin
+        user = raw_input("Username: ")
+        pw = getpass.getpass()
+elif len(sys.argv) == 3 and sys.argv[2] is "c":
+        #read .conf
+        parser = getConf(sys.argv[1])
+        path = os.path.expanduser(parser.get("Necessary", "path"))
+        from_address = parser.get("Necessary", "from")
+        smtp = parser.get("Necessary", "smtp")
+        port = parser.get("Necessary", "port")
+        to_address = parser.get("Necessary", "to")
+        ratio = float(parser.get("Optional", "ratio"))
+        sleepTime = float(parser.get("Optional", "sleeptime"))
+        exchange = parser.get("Optional", "exchange")
+        currency = parser.get("Optional", "currency")
+        buy_sell = parser.get("Optional", "buy_sell")
+        user = parser.get("Credentials", "user")
+        pw = parser.get("Credentials", "pw")
+else:
+        #read stdin
+        path = sys.argv[1]
+        from_address = sys.argv[2]
+        user = sys.argv[3]
+        pw = sys.argv[4]
+        smtp = sys.argv[5]
+        port = sys.argv[6]
+        to_address = sys.argv[7]
+        ratio = float(sys.argv[8])
+        sleepTime = float(sys.argv[9])
+        exchange = sys.argv[10]
+        currency = sys.argv[11]
+        buy_sell = sys.argv[12]
 while True:
-        main(path, from_address, user, pw, smtp, port, to_address, ratio)
+        main(path, from_address, user, pw, smtp, port, to_address, ratio, exchange, currency, buy_sell)
         time.sleep(sleepTime)
