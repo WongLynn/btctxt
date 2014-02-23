fdb9153ab1284b5e998bdecb982feeada39741b2
dmercer <mercer321@gmail.com>
2014-02-23 02:43:56 -0500

    PEP8, because I hate myself

--- btctxt.py
+++ btctxt.py
@@ -13,18 +13,17 @@ from email.mime.text import MIMEText
 
 class BTCtxt:
         def __init__(self):
-                self.last=1
-                self.from_address=""
-                self.smtp=""
-                self.port=""
-                self.to_address=""
-                self.user=""
-                self.pw=""
-                self.ratio=[]
-                self.sleeptime=[]
-                self.currency=""
+                self.last = 1
+                self.from_address = ""
+                self.smtp = ""
+                self.port = ""
+                self.to_address = ""
+                self.user = ""
+                self.pw = ""
+                self.ratio = []
+                self.sleeptime = []
+                self.currency = ""
                 return None
-
 
         def monitor(self):
                 #get current price
@@ -33,9 +32,8 @@ class BTCtxt:
                 change = abs((btctxt.last-current)/current)
                 if change > btctxt.ratio:
                         btctxt.sendEmail(current)
-                        btctxt.last=current
+                        btctxt.last = current
                 return self
-
 
         def sendEmail(self, current):
                 #Format email_content
@@ -75,15 +73,16 @@ def getConf(path):
                 elif item is "sleeptime":
                         parser.set("Optional", item, "180")
                 elif item is "ratio":
-                        parser.set("Optional", item, "0.075")          
+                        parser.set("Optional", item, "0.075")
         return parser
 
 
 if __name__ == '__main__':
         #Create instance of btctxt class
-        btctxt=BTCtxt()
+        btctxt = BTCtxt()
         #Populate btctxt attributes using one of three input methods
-        if len(sys.argv) == 2: #if only path to .conf file provided 
+        if len(sys.argv) == 2:
+        #if only path to .conf file provided
                 #read .conf
                 parser = getConf(sys.argv[1])
                 #write .conf to btctxt
@@ -97,7 +96,8 @@ if __name__ == '__main__':
                 #get user and pw from std in
                 btctxt.user = raw_input("Username: ")
                 btctxt.pw = getpass.getpass()
-        elif len(sys.argv) == 3 and sys.argv[2] is "c": #if path to .conf file and letter c provided as second argument
+        elif len(sys.argv) == 3 and sys.argv[2] is "c":
+        #if path to .conf file and letter c provided as second argument
                 #read .conf
                 parser = getConf(sys.argv[1])
                 #write .conf to btctxt
